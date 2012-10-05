04dab89afbda3e332c01963ceef1766aee9a98ab
sirfocus <sirfocus@a053ac88-d4ad-4f58-a43f-47ca826c7790>
2012-10-05 07:28:30 +0000

    Añado vista de informe
    git-svn-id: https://forja.rediris.es/svn/cleverfigures@2 a053ac88-d4ad-4f58-a43f-47ca826c7790

---
+++ application/views/content/check_results_view.php
@@ -0,0 +1,42 @@
+<!--
+	INPUT: i18n_content_evolution, i18n_activity, i18n_users, i18n_pages, i18n_categories, i18n_tag_cloud, content_evolution_chart
+		activity_chart, users_chart, pages_chart, categories_chart, tag_cloud, i18n_come_back
+	OUTPUT: back_home(generate_pdf_submit, go_home_submit)
+-->
+
+<!--Stats view-->
+
+<?	
+echo "<h3>$i18n_content_evolution</h3>";
+echo $content_evolution_chart;
+echo "</br>";
+
+echo "<h3>$i18n_activity</h3>";
+echo $activity_chart;
+echo "</br>";
+
+echo "<h3>$i18n_users</h3>";
+echo $users_chart;
+echo "</br>";
+
+echo "<h3>$i18n_pages</h3>";
+echo $pages_chart;
+echo "</br>";
+
+echo "<h3>$i18n_categories</h3>";
+echo $categories_chart;
+echo "</br>";
+
+echo "<h3>$i18n_tag_cloud</h3>";
+echo $tag_cloud;
+echo "</br>";
+
+echo open_form('back_home');
+echo form_submit('generate_pdf_submit', $i18n_generate_pdf); 
+// [2] www.christophermonnat.com/2008/08/generating-pdf-files-using-codeigniter
+echo form_submit('go_home_submit', $i18n_come_back);
+echo close_form();
+?>
+
+<!--[1] TO_DO: add more charts-->
+<!--[2] TO_DO: generate pdf-->
