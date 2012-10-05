52d4a3ebd8c5dd310f459f9c4109fd7afefd9552
sirfocus <sirfocus@a053ac88-d4ad-4f58-a43f-47ca826c7790>
2012-10-05 09:11:33 +0000

    Añado vista de fin de instalación
    git-svn-id: https://forja.rediris.es/svn/cleverfigures@4 a053ac88-d4ad-4f58-a43f-47ca826c7790

---
+++ application/views/content/installation4_view.php
@@ -0,0 +1,12 @@
+<!--
+	INPUT: i18n_installation_end, i18n_go_to_login
+	OUTPUT: installation_end_form(go_to_login_submit)
+-->
+
+<?
+echo $i18n_installation_end;
+
+echo form_open('installation_end_form');
+	<?= form_submit('go_to_login_submit', $i18n_go_to_login) ?>
+echo form_close();
+?>
