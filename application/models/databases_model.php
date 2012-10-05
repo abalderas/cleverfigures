562149cf284afcc5f2166debaf4f92dab2812d33
sirfocus <sirfocus@a053ac88-d4ad-4f58-a43f-47ca826c7790>
2012-10-05 23:37:26 +0000

    Añado modelos incompletos de gestion/control de las bases de datos, las gráficas, y uno para funciones varias que puedan surgir.
    git-svn-id: https://forja.rediris.es/svn/cleverfigures@8 a053ac88-d4ad-4f58-a43f-47ca826c7790

---
+++ application/models/databases_model.php
@@ -0,0 +1,37 @@
+<!--FUNCTIONS
+	function new_user(){}
+   	function fetch_wiki_data(){}
+   	function fetch_color_data(){}
+   	function save_data(){}
+   	function set_color_database_type(){}
+-->
+
+
+<?php
+
+class Databases_model extends CI_Model{
+   function Databases_model(){
+      	parent::__construct();
+      	/*[1]*/
+      	/*[2]*/
+   }
+   
+   function new_user(){}
+   function fetch_wiki_data(){}
+   function fetch_color_data(){}
+   function save_data(){}
+   function set_color_database_type(){}
+   
+   /*[3]*/
+}
+
+?> 
+
+<!--[1] TO_DO: you have to load all the databases, 
+but you have no idea of their names until you specify 
+how they'll be installed in the database.php file. 
+Must be done through another model-->
+
+<!--[2] TO_DO: must add helpers-->
+
+<!--[3] TO_DO: add parameters to created functions and maybe add more functions-->
