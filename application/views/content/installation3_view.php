96d15c04805e28534ee08e7dc665cc9768faa736
sirfocus <sirfocus@a053ac88-d4ad-4f58-a43f-47ca826c7790>
2012-10-05 23:13:25 +0000

    Añado bibliotecas de generación de gráficas. Modifico paso 2 de instalación: se añade la opción de elegir qué tipo de base de datos será la de coloreado.
    git-svn-id: https://forja.rediris.es/svn/cleverfigures@7 a053ac88-d4ad-4f58-a43f-47ca826c7790

--- application/views/content/installation3_view.php
+++ application/views/content/installation3_view.php
@@ -1,6 +1,6 @@
 <!--
 	INPUT: 	i18n_assessMW_database, i18n_cleverfigures_database, i18n_database_data, i18n_database_name, i18n_installation_databases
-		i18n_next, i18n_password, i18n_server, i18n_username, i18n_wiki_database
+		i18n_next, i18n_password, i18n_server, i18n_username, i18n_wiki_database, i18n_database_type
 	OUTPUT: connect_databases_form(server1, username1, password1, database_name1, server2, username2, password2, database_name2, 
 		server3, username3, password3, database_name3)
 -->
@@ -52,6 +52,18 @@ echo form_open('connect_databases_form');
 		<tr>
 			<td class="fieldbox"><?=$i18n_database_name?>:</td>
 			<td><?= form_input('database_name2') ?></td>
+		</tr>
+		<tr>
+			<td class="fieldbox"><?=$i18n_database_type?>:</td>
+			<?
+			$types = array(
+                  		'assess' => 'AssessMediaWiki',
+                  		// You can add more types here, but you'll have to create 
+                  		// new stuff somewhere to control it.
+                		);
+
+			echo form_dropdown('types_dropdown', $types, 'assess');
+			?>
 		</tr>
 		</table>	
 	</td>
