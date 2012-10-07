3f730b5b0fc49d692a2a4c15fbede81d400fc08e
sirfocus <sirfocus@a053ac88-d4ad-4f58-a43f-47ca826c7790>
2012-10-07 22:14:39 +0000

    Añado esquema de estructura de base de datos principal a la documentación. Añado parámetros a las funciones del modelo de control de bases de datos.
    git-svn-id: https://forja.rediris.es/svn/cleverfigures@11 a053ac88-d4ad-4f58-a43f-47ca826c7790

--- application/models/databases_model.php
+++ application/models/databases_model.php
@@ -10,19 +10,30 @@
 <?php
 
 class Databases_model extends CI_Model{
-   function Databases_model(){
-      	parent::__construct();
-      	/*[1]*/
-      	/*[2]*/
-   }
+
+   	function Databases_model(){
+   	   	parent::__construct();
+   	   	/*[1]*/
+   	   	/*[2]*/
+   	}
    
-   function new_user(){}
-   function fetch_wiki_data(){}
-   function fetch_color_data(){}
-   function save_data(){}
-   function set_color_database_type(){}
+   	// user functions
+   	function new_user($username, $password, $realname, $email){}
+   	function user_logged_off($username){}
+   	function delete_user($username){}
+   	
+   	//fetching functions
+   	function fetch_wiki_data(){}
+   	function fetch_color_data($database_type){}
+   	
+   	//analisis functions
+   	function save_analisis(){}
+   	function view_analisis($id){}
+   	
+   	//database configuration functions
+   	function set_color_database_type($database_type){}
    
-   /*[3]*/
+   	/*[3]*/
 }
 
 ?> 
