aa2e6748062b7360b09041ac0e6585302fca0ecd
sirfocus <sirfocus@a053ac88-d4ad-4f58-a43f-47ca826c7790>
2012-10-08 15:21:47 +0000

    Continuo metiendo código a los métodos del modelo de gestion de usuarios.
    git-svn-id: https://forja.rediris.es/svn/cleverfigures@14 a053ac88-d4ad-4f58-a43f-47ca826c7790

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,12 +1,49 @@
 <?php
 class User_model extends CI_Model{
 
+	//ATTIRBUTES
+	private $username;
+	private $password;
+	private $last_session;
+	private $realname;
+	private $nanalisis;
+	private $email;
+	private $configuration;
+	
+	//METHODS
    	function Users_model(){
    	   	parent::__construct();
+   	   	$this->load->helper('date');
+   	   	
+   	   	$username = 'default';
+   	   	$password = 'default';
+		$last_session = 0;
+		$realname = 'default';
+		$nanalisis = 0;
+		$email = 'default';
+		$configuration = 'default';
    	}
    	
-   	function save_new_user(){}
-   	function save_last_login(){}
+   	function set_user_data($id, $name, $pass, 0, $email_address){
+   		$this->load->model('Configuration_model');
+   		$this->username = $id;
+   		$this->password = $pass;
+   		$this->last_session = now();
+   		$this->realname = $name;
+   		$this->nanalisis = 0;
+   		$this->email = $email_address;
+   		$this->configuration = $id;
+   		
+   		$conf = new Configuration_model();
+   		$conf->set_configuration_language('english');
+   		$conf->set_configuration_date(now());
+   		$conf->set_wiki_database();
+   		$conf->set_color_database();
+   		
+   	}
+   	function save_user(){}
+   	function save_last_session(){}
+   	function login(){}
    	function delete_user(){}
 }
 
