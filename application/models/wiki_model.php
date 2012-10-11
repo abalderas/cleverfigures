c1d3c15cc89a192d777fca59af620780f67e3812
sirfocus <sirfocus@a053ac88-d4ad-4f58-a43f-47ca826c7790>
2012-10-11 23:49:58 +0000

    Añadido esquema de la base de datos de mediawiki por comodidad. Primeras consultas en el modelo de la wiki.
    git-svn-id: https://forja.rediris.es/svn/cleverfigures@32 a053ac88-d4ad-4f58-a43f-47ca826c7790

--- application/models/wiki_model.php
+++ application/models/wiki_model.php
@@ -7,10 +7,6 @@
 //    	fetch()
 //    	delete_wiki()
 
-//LOADED MODELS
-$ci =& get_instance();
-$ci->load->model('connection_model');
-
 
 class Wiki_model extends CI_Model{
 	
@@ -23,6 +19,9 @@ class Wiki_model extends CI_Model{
 	//constructor
    	function Wiki_model($id, $connection){
    	   	parent::__construct();
+   	   	$this->load->helper('date');
+   	   	$ci =& get_instance();
+		$ci->load->model('connection_model');
    	   	
 		$this->wiki_id = $id;
 		$this->wiki_connection = $connection;
@@ -45,8 +44,56 @@ class Wiki_model extends CI_Model{
    	
    	function connection(){return $this->wiki_connection;}
    	
-   	function fetch(){
-   		//todo
+   	function fetch_general_stats(){
+   		return $this->connection()->get_query("SELECT ss_total_views, ss_total_edits, ss_good_articles, ss_total_pages, ss_users, ss_active_users, ss_admins, ss_images FROM site_stats LIMIT 1");
+   	}
+   	
+   	function fetch_linked_categories($date_range_a => 'default', $date_range_b => 'default'){
+   		if($date_range_a == 'default'){
+   			$initial_date = $this->connection()->get_query("SELECT rev_timestamp FROM revision ORDER BY rev_timestamp ASC LIMIT 1");
+   			if($initial_date->num_rows() != 0)
+   				foreach ($initial_date as $row)
+   					$date_range_a = $row -> rev_timestamp;
+   			else
+   				return false;
+   		}
+   		
+   		if($date_range_b == 'default')
+   			$date_range_b = date('Y-m-d H:i:s', now());
+   		
+   		$result = $this->connection()->get_query("SELECT cl_from, cl_to FROM scategorylinks WHERE cl_from IN (SELECT DISTINCT rev_page FROM revision WHERE rev_timestamp>=$date_range_a and rev_timestamp<=$date_range_b)");
+   		
+   		$counter = 0;
+   		foreach ($result as $row){
+   			$array[$row->cl_from] = $row->cl_to;
+   			$counter++;
+   		}
+   		
+   		return $array; //$[page id] = category name
+   	}
+   	
+   	function fetch_all_categories($date_range_a => 'default', $date_range_b => 'default'){
+   		if($date_range_a == 'default'){
+   			$initial_date = $this->connection()->get_query("SELECT rev_timestamp FROM revision ORDER BY rev_timestamp ASC LIMIT 1");
+   			if($initial_date->num_rows() != 0)
+   				foreach ($initial_date as $row)
+   					$date_range_a = $row -> rev_timestamp;
+   			else
+   				return false;
+   		}
+   		
+   		if($date_range_b == 'default')
+   			$date_range_b = date('Y-m-d H:i:s', now());
+   		
+   		$result = $this->connection()->get_query("SELECT page_title FROM page WHERE page_namespace=14 AND page_id IN (SELECT DISTINCT rev_page FROM revision WHERE rev_timestamp>=$date_range_a and rev_timestamp<=$date_range_b)");
+   		
+   		$counter = 0;
+   		foreach ($result as $row){
+   			$array[$counter] = $row->page_title;
+   			$counter++;
+   		}
+   		
+   		return $array; //$[index] = page title
    	}
    	
    	function delete_wiki(){
