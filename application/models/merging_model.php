e753e82b8dc1232aee1d276c8e1d3588ee8769fd
sirfocus <sirfocus@a053ac88-d4ad-4f58-a43f-47ca826c7790>
2012-10-22 23:28:58 +0000

    Comenzado modelo de merging. Eliminado modelo de sentiment. Se añadirá como tarea pendiente de baja prioridad.
    git-svn-id: https://forja.rediris.es/svn/cleverfigures@54 a053ac88-d4ad-4f58-a43f-47ca826c7790

--- application/models/merging_model.php
+++ application/models/merging_model.php
@@ -1,1 +1,27 @@
  
+<?php
+
+//AVAILABLE METHODS
+
+
+class Merging_model extends CI_Model{
+	
+	
+   	function Merging_model(){
+   		//Llamamos al constructor heredado.
+   	   	parent::__construct();
+   	   	
+   	   	//Cargamos helpers
+   	   	$this->load->helper('date');
+   	   	
+   	   	//Cargamos models necesarios
+   	   	$ci =& get_instance();
+		$ci->load->model('connection_model');
+   	}
+   	
+   	function general_fetch(){
+		///////////////////////////////////////////////////////
+   	}
+}
+
+?> 
