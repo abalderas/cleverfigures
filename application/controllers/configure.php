29f7d54eee046c62efe6a3264dfbecc537a7d265
sirfocus <sirfocus@a053ac88-d4ad-4f58-a43f-47ca826c7790>
2012-11-01 23:00:19 +0000

    Creado controlador de configuración, analisis, analisis realizados y cerrar sesión.
    git-svn-id: https://forja.rediris.es/svn/cleverfigures@79 a053ac88-d4ad-4f58-a43f-47ca826c7790

---
+++ application/controllers/configure.php
@@ -0,0 +1,33 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// <<Copyright 2013 Alvaro Almagro Doello>>
+// 
+// This file is part of CleverFigures.
+// 
+// CleverFigures is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// CleverFigures is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with CleverFigures.  If not, see <http://www.gnu.org/licenses/>.
+
+class Select_language_form extends CI_Controller {
+
+	function Select_language_form(){
+      		parent::__construct();
+   	}
+   	
+	function index(){
+      		
+		$datah = array('title' => lang('voc.i18n_configuration'));
+		$this->load->view('templates/header_view', $datah);
+		$this->load->view('content/configuration_view');
+		$this->load->view('templates/footer_view');
+	}
+} 
