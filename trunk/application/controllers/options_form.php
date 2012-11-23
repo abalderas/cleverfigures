05cca4f1da2b235a06be221633ec4defe5780a06
sirfocus <sirfocus@a053ac88-d4ad-4f58-a43f-47ca826c7790>
2012-11-23 22:48:42 +0000

    Avanzando la función de obtención de tablas de datos para los informes.
    git-svn-id: https://forja.rediris.es/svn/cleverfigures@117 a053ac88-d4ad-4f58-a43f-47ca826c7790

--- trunk/application/controllers/options_form.php
+++ trunk/application/controllers/options_form.php
@@ -32,6 +32,73 @@ class Options_form extends CI_Controller {
 // 		$this->lang->load('voc', $this->session->userdata('language'));
    	}
    	
+   	function get_main_tables($analisis){
+		
+		//USER TABLE
+		
+		$str = "<table id = \"bodytable\">
+		<tr>
+			<th>User</th>
+			<th>Real Name</th>
+			<th>Editions</th>
+			<th>%</th>
+			<th>Editions in articles</th>
+			<th>%</th>
+			<th>Bytes</th>
+			<th>%</th>
+			<th>Bytes in articles</th>
+			<th>%</th>
+			<th>Uploads</th>
+			<th>%</th>
+			<th>Created Pages</th>
+		</tr>";
+		
+		$ids = $this->csv_model->csv_to_array($analisis, 'iduser');
+		$rnames = $this->csv_model->csv_to_array($analisis, 'userrealname');
+		$edits = $this->csv_model->csv_to_array($analisis, 'useredits');
+		$edits_per = $this->csv_model->csv_to_array($analisis, 'useredits_per');
+		$edits_art = $this->csv_model->csv_to_array($analisis, 'useredits_art');
+		$edits_art_per = $this->csv_model->csv_to_array($analisis, 'useredits_art_per');
+		$bytes = $this->csv_model->csv_to_array($analisis, 'userbytes');
+		$bytes_per = $this->csv_model->csv_to_array($analisis, 'userbytes_per');
+		$uploads = $this->csv_model->csv_to_array($analisis, 'useruploads');
+		$uploads_per = $this->csv_model->csv_to_array($analisis, 'useruploads_per');
+		$creadtedp = $this->csv_model->csv_to_array($analisis, 'usercreatedpages'.);
+		$bytes_art = $this->csv_model->csv_to_array($analisis, 'userbytes_art');
+		$bytes_art_per = $this->csv_model->csv_to_array($analisis, 'userbytes_art_per');
+		
+		foreach(array_keys($ids) as $key){
+		
+			$userdata
+		
+			$str .= "<tr>";
+			$str .= "<td>".$key."</td>";
+			$str .= "<td>".$rnames[$key]."</td>";
+			$str .= "<td>".end($edits[$key])."</td>";
+			$str .= "<td>".end($edits_per[$key])."</td>";
+			$str .= "<td>".end($edits_art[$key])."</td>";
+			$str .= "<td>".end($edits_art_per[$key])."</td>";
+			$str .= "<td>".end($bytes[$key])."</td>";
+			$str .= "<td>".end($bytes_per[$key])."</td>";
+			$str .= "<td>".end($bytes_art[$key])."</td>";
+			$str .= "<td>".end($bytes_art_per[$key])."</td>";
+			$str .= "<td>".end($uploads[$key])."</td>";
+			$str .= "<td>".end($uploads_per[$key])."</td>";
+			$str .= "<td>".$creadtedp[$key]."</td>";
+			$str .= "</tr>";
+		}
+		
+		$str .= "</table>";
+		
+		
+		//PAGE TABLE
+		
+		
+		//CATEGORY TABLE
+		
+		return "hey";
+	}
+	
    	function index(){
    	
 		if(!$this->session->userdata('username')){
@@ -52,6 +119,7 @@ class Options_form extends CI_Controller {
 			}
 			else if(isset($_POST['view'])){
 				$datah = array('title' => lang('voc.i18n_check_results'));
+				
 				
 				$this->load->view('templates/header_view', $datah);
 				$this->load->view('content/check_results_view', $_POST['alabel']);
