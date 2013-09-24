a53cba6fb1105106a77617c1b05bb03fc662aeb7
sirfocus <sirfocus@a053ac88-d4ad-4f58-a43f-47ca826c7790>
2013-09-24 21:23:04 +0000

    Adding database configuration script
    git-svn-id: https://forja.rediris.es/svn/cleverfigures@207 a053ac88-d4ad-4f58-a43f-47ca826c7790

--- trunk/scripts/install.sh
+++ trunk/scripts/install.sh
@@ -1,9 +1,13 @@
-## Installs cleverfigures in the system
+#############################################################################
+##                 INSTALLS CLEVERFIGURES IN THE SYSTEM                    ##
+#############################################################################
 
-## Sets variables
+
+## Sets paths
 CLEVERFIGURES_INSTALLATION_PATH=
 CLEVERFIGURES_DOWNLOADED_FOLDER=
 
+## Sets mysql variables
 MYSQL_COMMAND=
 MYSQLUSER=
 MYSQLPASSWORD=
@@ -12,9 +16,30 @@ MYSQLDBNAME=
 SQLFOLDER=
 
 
+## ----------------------------------------------------------------------- ##
+##                      DO NOT EDIT BELOW THIS LINE                        ##
+## ----------------------------------------------------------------------- ##
+
+
+## Checks that paths have been set
+echo "Checking neccessary variables..."
+if [ ! -d $CLEVERFIGURES_INSTALLATION_PATH ]; then
+  echo "ERROR: You must set Cleverfigures installation path correctly"
+  exit 1
+fi
+
+if [ ! -d $CLEVERFIGURES_DOWNLOADED_FOLDER ]; then
+  echo "ERROR: You must set Cleverfigures package path correctly"
+  exit 1
+fi
+echo "OK\n"
+
 ## Check mysql status
 echo "Checking mysql binnary..."
-hash $MYSQL_COMMAND 2>/dev/null || { echo >&2 "No pudo ejecutarse Mysql con el comando dado"; exit 1; }
+hash $MYSQL_COMMAND 2>/dev/null || {
+  echo >&2 "ERROR: No pudo ejecutarse Mysql con el comando dado"
+  exit 1
+}
 echo "OK\n"
 
 ## Create database
@@ -25,21 +50,21 @@ echo "OK\n"
 ## Install cleverfigures
 echo "Copying files..."
 cp $CLEVERFIGURES_DOWNLOADED_FOLDER $CLEVERFIGURES_INSTALLATION_PATH
-mv $CLEVERFIGURES_INSTALLATION_PATH/application/config/database.php $CLEVERFIGURES_INSTALLATION_PATH/application/config/database.php.old
 echo "OK\n"
 
 echo "Configuring database..."
-bash $SQLFOLDER/databaseconfig.sh $MYSQLSERVER $MYSQLDBNAME $MYSQLUSER $MYSQLPASSWORD
+mv $CLEVERFIGURES_INSTALLATION_PATH/application/config/database.php $CLEVERFIGURES_INSTALLATION_PATH/application/config/database.php.old
+bash $SQLFOLDER/databaseconfig.sh $MYSQLSERVER $MYSQLDBNAME $MYSQLUSER $MYSQLPASSWORD $CLEVERFIGURES_INSTALLATION_PATH/application/config/
 echo "OK\n"
 
-echo "Creating neccessary files..."
-
+echo "Creating neccessary folders..."
 if [ ! -d $CLEVERFIGURES_INSTALLATION_PATH/application/analysis ]; then
   mkdir $CLEVERFIGURES_INSTALLATION_PATH/application/analysis
 fi
+
 if [ ! -d $CLEVERFIGURES_INSTALLATION_PATH/application/csv ]; then
   mkdir $CLEVERFIGURES_INSTALLATION_PATH/application/csv
 fi
 echo "OK\n"
 
-echo ">> Cleverfigures has been installed <<"
+echo ">> Cleverfigures has been installed successfully <<"
