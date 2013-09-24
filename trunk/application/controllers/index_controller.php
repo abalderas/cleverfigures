c50bdb8d8f2520a9c7d3ecea4ac6da81bcc6a1fc
sirfocus <sirfocus@a053ac88-d4ad-4f58-a43f-47ca826c7790>
2013-09-24 22:22:31 +0000

    Modifying login to be adapted to new scripts
    git-svn-id: https://forja.rediris.es/svn/cleverfigures@208 a053ac88-d4ad-4f58-a43f-47ca826c7790

--- trunk/application/controllers/index_controller.php
+++ trunk/application/controllers/index_controller.php
@@ -1,19 +1,19 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 // <<Copyright 2013 Alvaro Almagro Doello>>
-// 
+//
 // This file is part of CleverFigures.
-// 
+//
 // CleverFigures is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // CleverFigures is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with CleverFigures.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -21,56 +21,21 @@
 //GROUP CREATE ARRAY
 class Index_controller extends CI_Controller {
 
-	function Index_controller(){
-		parent::__construct();
-		$this->load->model('analisis_model');
-		$this->load->model('dbforge_model');
-		$this->load->model('user_model');
-		$this->load->dbutil();
-   	   	$this->load->helper('file');
- 	}
-   	
- 	function index(){
-			
-		//IF FIRST TIME RUNNING CLEVERFIGURES
-		if(!strpos(read_file('application/config/database.php'),'cleverfigures') !== false){
-		
-			$this->session->sess_destroy();
-			$this->lang->load('voc', 'english');
-			
-			//CREATE HEADER ARRAY
-			$datah = array('title' => lang('voc.i18n_installation'));
-			
-			//LOAD INSTALLATION VIEW
-			$this->load->view('templates/header_view', $datah);
-			$this->load->view('content/installation1_view');
-			$this->load->view('templates/footer_view');
-		}
-		//IF NOT, CHECK ACTIVE SESSION
-		else{
-			//IF USER LOGGED IN
-			if($this->session->userdata('username')){
-				if(!$this->session->userdata('is_student'))
-					redirect('teacher');
-				else
-					redirect('student');
-			}
-			else{
-				$this->lang->load('voc', 'english');
-				
-				//IF AN USER ALREADY EXISTS
-				if($this->db->query('select * from user')->result())
-					redirect('login/loadlogin/');
-				else{
-					//CREATE HEADER ARRAY
-					$datah = array('title' => lang('voc.i18n_installation'));
-			
-					//LOAD USER CREATION VIEW
-					$this->load->view('templates/header_view', $datah);
-					$this->load->view('content/installation2_view');
-					$this->load->view('templates/footer_view');
-				}
-			}
-		}
-	}
+  function Index_controller(){
+    parent::__construct();
+  }
+
+  function index(){
+    //IF USER LOGGED IN
+    if($this->session->userdata('username')){
+      if(!$this->session->userdata('is_student'))
+        redirect('teacher');
+      else
+        redirect('student');
+    }
+    else{
+      $this->lang->load('voc', 'english');
+      redirect('login/loadlogin/');
+    }
+  }
 }
--- trunk/application/controllers/login.php
+++ trunk/application/controllers/login.php
@@ -1,19 +1,19 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 // <<Copyright 2013 Alvaro Almagro Doello>>
-// 
+//
 // This file is part of CleverFigures.
-// 
+//
 // CleverFigures is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // CleverFigures is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with CleverFigures.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -21,21 +21,21 @@
 //LOGIN CONTROLLER
 class Login extends CI_Controller {
 
-	function Login(){
-		parent::__construct();
-		$this->lang->load('voc', 'english');
- 	}
-   	
-	function loadlogin($error = false){
-		//CREATE HEADER ARRAY
-		$datah = array('title' => lang('voc.i18n_login'));
-		
-		//LOAD LOGIN VIEW
-		$this->load->view('templates/header_view', $datah);
-		if($error)
-			$this->load->view('content/login_view', array('error' => $error));
-		else
-			$this->load->view('content/login_view');
-		$this->load->view('templates/footer_view');
-	}
+  function Login(){
+    parent::__construct();
+    $this->lang->load('voc', 'english');
+  }
+
+  function loadlogin($error = false){
+    //CREATE HEADER ARRAY
+    $datah = array('title' => lang('voc.i18n_login'));
+
+    //LOAD LOGIN VIEW
+    $this->load->view('templates/header_view', $datah);
+    if($error)
+      $this->load->view('content/login_view', array('error' => $error));
+    else
+      $this->load->view('content/login_view');
+    $this->load->view('templates/footer_view');
+  }
 }
