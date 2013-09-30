61a69e248c13332f2ccd3e4916188c9e3f3fb681
sirfocus <sirfocus@a053ac88-d4ad-4f58-a43f-47ca826c7790>
2013-09-30 19:21:42 +0000

    Modifies user view to adapt it to general look
    git-svn-id: https://forja.rediris.es/svn/cleverfigures@218 a053ac88-d4ad-4f58-a43f-47ca826c7790

--- trunk/application/views/content/create_user_view.php
+++ trunk/application/views/content/create_user_view.php
@@ -18,49 +18,42 @@
 // along with CleverFigures.  If not, see <http://www.gnu.org/licenses/>.
 ?>
 
-<table id = "maintable">
+<?= form_open('create_user_form');?>
+<table id = "bodytable">
 <tr>
-	<td><?= img('images/logo/logotrans.png');?></td>
-	<td>
-		<?= form_open('create_user_form');?>
-		<table id = "formins">
-		<tr>
-			<th colspan = "2" ><?=lang('voc.i18n_create_user')?></th>
-		</tr>
-		<tr>
-			<td class="fieldbox"><?=lang('voc.i18n_email')?>:</td>
-			<td><?= form_input('email') ?></td>
-		</tr>
-		<?php if(form_error('email')) echo "<tr><td><em>".form_error('email')."</em></td></tr>"; ?>
-		<tr>
-			<td class="fieldbox"><?=lang('voc.i18n_username')?>:</td>
-			<td><?= form_input('username') ?></td>
-		</tr>
-		<?php if(form_error('username')) echo "<tr><td><em>".form_error('username')."</em></td></tr>"; ?>
-		<tr>
-			<td class="fieldbox"><?=lang('voc.i18n_real_name')?>:</td>
-			<td><?= form_input('real_name') ?></td>
-		</tr>
-		<?php if(form_error('real_name')) echo "<tr><td><em>".form_error('real_name')."</em></td></tr>"; ?>
-		<tr>
-			<td class="fieldbox"><?=lang('voc.i18n_password')?>:</td>
-			<td><?= form_password('password') ?></td>
-		</tr>
-		<?php if(form_error('password')) echo "<tr><td><em>".form_error('password')."</em></td></tr>"; ?>
-		<tr>
-			<td class="fieldbox"><?=lang('voc.i18n_retype_password')?>:</td>
-			<td><?= form_password('password_confirmation') ?></td>
-		</tr>
-		<?php if(form_error('password_confirmation')) echo "<tr><td><em>".form_error('password_confirmation')."</em></td></tr>"; ?>
-		<tr>
-			<td class="fieldbox"><?=lang('voc.i18n_is_admin')?>:</td>
-			<td><?= form_checkbox('is_admin') ?></td>
-		</tr>
-		<tr>
-			<th colspan = "2"><?=form_submit(array('id' => 'submit', 'value' => lang('voc.i18n_next')));?></th>
-		</tr>
-		</table>
-		<?=form_close();?>
-	</td>
+	<th colspan = "2" ><?=lang('voc.i18n_create_user')?></th>
+</tr>
+<tr>
+	<td class="fieldbox"><?=lang('voc.i18n_email')?>:</td>
+	<td><?= form_input('email') ?></td>
+</tr>
+<?php if(form_error('email')) echo "<tr><td><em>".form_error('email')."</em></td></tr>"; ?>
+<tr>
+	<td class="fieldbox"><?=lang('voc.i18n_username')?>:</td>
+	<td><?= form_input('username') ?></td>
+</tr>
+<?php if(form_error('username')) echo "<tr><td><em>".form_error('username')."</em></td></tr>"; ?>
+<tr>
+	<td class="fieldbox"><?=lang('voc.i18n_real_name')?>:</td>
+	<td><?= form_input('real_name') ?></td>
+</tr>
+<?php if(form_error('real_name')) echo "<tr><td><em>".form_error('real_name')."</em></td></tr>"; ?>
+<tr>
+	<td class="fieldbox"><?=lang('voc.i18n_password')?>:</td>
+	<td><?= form_password('password') ?></td>
+</tr>
+<?php if(form_error('password')) echo "<tr><td><em>".form_error('password')."</em></td></tr>"; ?>
+<tr>
+	<td class="fieldbox"><?=lang('voc.i18n_retype_password')?>:</td>
+	<td><?= form_password('password_confirmation') ?></td>
+</tr>
+<?php if(form_error('password_confirmation')) echo "<tr><td><em>".form_error('password_confirmation')."</em></td></tr>"; ?>
+<tr>
+	<td class="fieldbox"><?=lang('voc.i18n_is_admin')?>:</td>
+	<td><?= form_checkbox('is_admin') ?></td>
+</tr>
+<tr>
+	<th colspan = "2"><?=form_submit(array('id' => 'submit', 'value' => lang('voc.i18n_next')));?></th>
 </tr>
 </table>
+<?=form_close();?>
